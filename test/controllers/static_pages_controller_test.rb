01a4ada8e630423719c56217c4745409c5751faf
SpiffySaxMan <willbennett822@gmail.com>
2015-12-05 14:46:44 -0600

    CSS Stylesheets edits and fixes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ridealong App"
+    assert_select "title", "Ridealong App"
   end
 
   test "should get help" do
