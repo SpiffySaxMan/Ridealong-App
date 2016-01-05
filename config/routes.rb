711fdd9371bfebe5fc1ca08d7ffa20971cf4bc5a
SpiffySaxMan <willbennett822@gmail.com>
2016-01-05 15:29:40 -0500

    bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :layouts
   resources :users
-  resources :microposts,          only: [:create, :destroy]
+  resources :microposts
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
 end
